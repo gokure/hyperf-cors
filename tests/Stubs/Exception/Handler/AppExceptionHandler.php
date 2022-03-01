e72d20a91dc8b05784a65352cdb53c6ed111c996
Gang Wu <gokure@gmail.com>
2022-03-01 16:47:00 +0800

    cs fix

--- tests/Stubs/Exception/Handler/AppExceptionHandler.php
+++ tests/Stubs/Exception/Handler/AppExceptionHandler.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Gokure\HyperfCors\Tests\Stubs\Exception\Handler;
 
-
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Hyperf\Validation\ValidationException;
